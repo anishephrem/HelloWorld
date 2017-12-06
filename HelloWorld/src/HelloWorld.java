d2a0c0b2be3f61b08a402e30be393a4c33061945
Anish Ephrem <anishephrem@gmail.com>
2017-12-06 14:11:57 +0530

    Added comment

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		// Comment section here
+		// Adding comment for hello world
 		System.out.println("Hello World");
 		
 	}
